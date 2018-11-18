43865295a648fb549fd8371fdf6a88401009c162
phillmac <phillmac@users.noreply.github.com>
2018-11-18 18:31:37 +1100

    Add function create_split_files
    Move file creation to a seperate function and export resulting arrays

--- scripts/util_functions.sh
+++ scripts/util_functions.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
 function wget_split_output () {
-    wget_url="${1}"
-    output_count="${2}"
+    wget_url="${1}    
+    wget "${wget_url}" -O - | tee ${tee_output_list} > /dev/null
+}
+
+function create_split_files () {
+    output_count="${1}"
     
     tmpdir="$(mktemp)"
     
-    output_file_list=
+    output_file_list=()
+    tee_output_list=()
     
     for ((i=1; i<=output_count; i++))
     do
         output_file="${tmpdir}/output/${i}"
         mkfifo "${output_file}"
-        add_end output_file_list ">(cat > ${output_file})"
-    done 
+        add_end tee_output_list ">(cat > ${output_file})"
+        add_end output_file_list
+    done
     
-    wget "${wget_url}" -O - | tee ${output_file_list} > /dev/null
+    export output_file_list tee_output_list
 }
