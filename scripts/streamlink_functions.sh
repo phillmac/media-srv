6356dede5ddc867ee96113a40febeeb8dc1ba535
phillmac <phillmac@users.noreply.github.com>
2018-11-18 14:59:23 +1100

    Fix default value issues

--- scripts/streamlink_functions.sh
+++ scripts/streamlink_functions.sh
@@ -2,11 +2,12 @@
 
 function streamlink_http_out () {
 
-streamlink_url="{1}"
-streamlink_quality="{2}"
+streamlink_url="${1}"
+streamlink_quality="${2}"
 
-streamlink_http_port="${STREAMLINK_HTTP_PORT:-9000}"
-streamlink_http_port="${3:streamlink_http_port}"
+streamlink_http_port="9000"
+streamlink_http_port="${STREAMLINK_HTTP_PORT:-${streamlink_http_port}}"
+streamlink_http_port="${3:-${streamlink_http_port}}"
 
 streamlink \
     --player-external-http \
