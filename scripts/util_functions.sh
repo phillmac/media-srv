bca12a59376b726fb16fba2d6d19325da423bd70
phillmac <phillmac@users.noreply.github.com>
2018-11-18 21:41:15 +1100

    Add function split_output

--- scripts/util_functions.sh
+++ scripts/util_functions.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+function split_output () {
+    tee ${output_file_list[@]} > /dev/null
+}
+
 function wget_split_output () {
     wget_url="${1}"
-    output_count="${2}"
-    echo "${output_file_list[@]}"
-    wget "${wget_url}" -O - | tee ${output_file_list[@]} > /dev/null
+    wget "${wget_url}" -O - | split_output
 }
 
 function create_split_files () {
