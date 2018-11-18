a6e0de5e6813f0c28aa850b4984a602fd1555baf
phillmac <phillmac@users.noreply.github.com>
2018-11-18 18:04:04 +1100

    load array_functions.sh

--- scripts/load_functions.sh
+++ scripts/load_functions.sh
@@ -2,6 +2,7 @@
 
 scripts_dir="$(dirname $(realpath ${BASH_SOURCE}))" 
 
+source "${scripts_dir}/array_functions.sh"
 source "${scripts_dir}/mkv_server_functions.sh"
 source "${scripts_dir}/streamlink_functions.sh"
 source "${scripts_dir}/util_functions.sh"
