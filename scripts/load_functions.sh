53fceba44b913010223505e2fe4d8148bc4eacb6
phillmac <phillmac@users.noreply.github.com>
2018-11-18 18:01:52 +1100

    Create load_functions.sh
    load files 
    mkv_server_functions.sh
    streamlink_functions.sh
    util_functions.sh

---
+++ scripts/load_functions.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+scripts_dir="$(dirname $(realpath ${BASH_SOURCE}))" 
+
+source "${scripts_dir}/mkv_server_functions.sh"
+source "${scripts_dir}/streamlink_functions.sh"
+source "${scripts_dir}/util_functions.sh"
