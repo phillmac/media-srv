c00d448fcffa76228e7b6cb903df63ebca69108e
phillmac <phillmac@users.noreply.github.com>
2018-11-18 22:31:09 +1100

    Export file params

--- scripts/media_functions.sh
+++ scripts/media_functions.sh
@@ -10,6 +10,8 @@ function streamlink_in_out_hls_out_mkvserver () {
     ffmpeg_hls_in="${output_file_list[0]}"
     mkvserver_ffmpeg_in="${output_file_list[1]}"
     
+    export ffmpeg_hls_in mkvserver_f fmpeg_in ffmpeg_hls_out
+    
     tmux split bash -c "ffmpeg_hls_out ${ffmpeg_hls_in} ${ffmpeg_hls_out}"
     tmux split bash -c "mvkserver_out_aac_audio ${mkvserver_ffmpeg_in}"
     streamlink_stdout "${streamlink_url}" "${streamlink_quality}" | split_output
