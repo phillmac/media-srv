29e505b53e88532b8fe6fa2a3f043af99aac45aa
phillmac <phillmac@users.noreply.github.com>
2018-11-18 21:46:02 +1100

    Add param for ffmpeg_hls_out

--- scripts/media_functions.sh
+++ scripts/media_functions.sh
@@ -3,13 +3,14 @@
 function streamlink_in_out_hls_out_mkvserver () {
     streamlink_url="${1}"
     streamlink_quality="${2}"
+    ffmpeg_hls_out="${3}"
     
     create_split_files 2
     
     ffmpeg_hls_in="${output_file_list[1]}"
     ffmpeg_mkvserver_in="${output_file_list[2]}"
     
-    tmux split ffmpeg_hls_out "${ffmpeg_hls_in}"
+    tmux split ffmpeg_hls_out "${ffmpeg_hls_in}"  "${ffmpeg_hls_out}"
     tmux split mvkserver_out_aac_audio "${ffmpeg_mkvserver_in}"
     streamlink_stdout | split_output
 }
