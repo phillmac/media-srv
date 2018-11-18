22ecb9ffb78277d3ae3308f150217e6fcfe64367
phillmac <phillmac@users.noreply.github.com>
2018-11-18 21:43:42 +1100

    Source new files

--- scripts/load_functions.sh
+++ scripts/load_functions.sh
@@ -3,6 +3,8 @@
 scripts_dir="$(dirname $(realpath ${BASH_SOURCE}))" 
 
 source "${scripts_dir}/array_functions.sh"
+source "${scripts_dir}/ffmpeg_functions.sh"
+source "${scripts_dir}/media_functions.sh"
 source "${scripts_dir}/mkv_server_functions.sh"
 source "${scripts_dir}/streamlink_functions.sh"
 source "${scripts_dir}/util_functions.sh"
