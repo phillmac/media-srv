15039fbbdfb368829b1a22dc1ba8c699044e32c6
phillmac <phillmac@users.noreply.github.com>
2018-11-18 16:18:54 +1100

    Fix file name

--- scripts/mkv_server_functions.sh
+++ scripts/mkv_server_functions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function serve_stream () {
-  ffmpeg -re -i  http://127.0.0.1:9000/ -c:v copy -c:a libmp3lame -q:a 0 -f matroska - | server 2>/dev/null 1>/dev/null
+  ffmpeg -re -i  http://127.0.0.1:9000/ -c:v copy -c:a libmp3lame -q:a 0 -f matroska - | mkv_server 2>/dev/null 1>/dev/null
 }
 
 
