0e7dc40d3fb43f44ba0e5fd893d6448e54d05450
phillmac <phillmac@users.noreply.github.com>
2018-11-18 16:12:04 +1100

    Fix missing \

--- scripts/streamlink_functions.sh
+++ scripts/streamlink_functions.sh
@@ -10,9 +10,9 @@ streamlink_http_port="${STREAMLINK_HTTP_PORT:-${streamlink_http_port}}"
 streamlink_http_port="${3:-${streamlink_http_port}}"
 
 streamlink \
-    --hls-segment-threads 3
-    --hls-live-edge 10
-    --ringbuffer-size 256M
+    --hls-segment-threads 3 \
+    --hls-live-edge 10 \
+    --ringbuffer-size 256M \
     --player-external-http \
     --player-external-http-port "${streamlink_http_port}" \
     "${streamlink_url}" "${streamlink_quality}"
