29b7c58c27743c5a19bc07f8ac8cf9a1e02b25ec
phillmac <phillmac@users.noreply.github.com>
2018-11-18 19:51:27 +1100

    Fix problems, cleanup

--- scripts/util_functions.sh
+++ scripts/util_functions.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
 
 function wget_split_output () {
-    wget_url="${1}"  
-    wget "${wget_url}" -O - | tee ${tee_output_list} > /dev/null
+    wget_url="${1}"
+    output_count="${2}"
+    echo "${output_file_list[@]}"
+    wget "${wget_url}" -O - | tee ${output_file_list[@]} > /dev/null
 }
 
 function create_split_files () {
     output_count="${1}"
     
     tmpdir="$(mktemp -d)"
-    
-    output_file_list=()
-    tee_output_list=()
-    
+    unset output_file_list
+        
     for ((i=1; i<=output_count; i++))
     do
         output_file="${tmpdir}/output_${i}"
         mkfifo "${output_file}"
-        add_end tee_output_list ">(cat > ${output_file})"
-        add_end output_file_list
+        add_end output_file_list "${output_file}"
     done
-    
-    export output_file_list tee_output_list
 }
 
 export -f wget_split_output
