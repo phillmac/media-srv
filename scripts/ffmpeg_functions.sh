4028c826d76b01a84c1f4f8cd1a58623ea047e1d
phillmac <phillmac@users.noreply.github.com>
2018-11-18 22:25:03 +1100

    Debug stalled ffmpeg

--- scripts/ffmpeg_functions.sh
+++ scripts/ffmpeg_functions.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 function ffmpeg_hls_out () {
-    stream_in="${1}"
-    output_file="${2}"
+    ffmpeg_hls_in="-"
+    ffmpeg_hls_in="${1:-${ffmpeg_hls_in}}"
+    echo "ffmpeg input: ${ffmpeg_hls_in}"
     
-    ffmpeg -re -i "${stream_in}" -c copy -segment_list_size 10 -hls_time 10 -hls_flags delete_segments "${output_file}"
+    ffmpeg_hls_out="${2:-${ffmpeg_hls_out}}"
+    echo "ffmpeg output: ${ffmpeg_hls_out}"
+    
+    ffmpeg -re -i "${ffmpeg_hls_in}" -c copy -segment_list_size 10 -hls_time 10 -hls_flags delete_segments "${ffmpeg_hls_out}"
 }
 
 export -f ffmpeg_hls_out
