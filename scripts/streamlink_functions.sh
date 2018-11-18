d06fb471a3f0ce3515153244759903b2ee70129f
phillmac <phillmac@users.noreply.github.com>
2018-11-18 14:19:51 +1100

    Add function streamlink_http_out

---
+++ scripts/streamlink_functions.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+function streamlink_http_out () {
+
+streamlink_url="{1}"
+streamlink_quality="{2}"
+
+streamlink_http_port="${STREAMLINK_HTTP_PORT:-9000}"
+streamlink_http_port="${3:streamlink_http_port}"
+
+streamlink \
+    --player-external-http
+    --player-external-http-port "${streamlink_http_port}" \
+    "${streamlink_url}" "${streamlink_quality}"
+
+}
