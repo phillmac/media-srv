204c594ffce83751c71332e5ba5927e53c157c62
phillmac <phillmac@users.noreply.github.com>
2018-11-18 21:25:53 +1100

    Add functions, fix default params
    mvkserver_out_aac_audio
    mvkserver_out_passthru_audio

--- scripts/mkv_server_functions.sh
+++ scripts/mkv_server_functions.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
 function mvkserver_out_mp3_audio () {
-  mkvserver_ffmpeg_in="${1}"
-  ffmpeg -re -i "${mkvserver_ffmpeg_in}" -c:v copy -c:a libmp3lame -q:a 0 -f matroska - | mkv_server 2>/dev/null 1>/dev/null
+    mkvserver_ffmpeg_in="-"
+    mkvserver_ffmpeg_in="${1:-${mkvserver_ffmpeg_in}}"
+    ffmpeg -re -i "${mkvserver_ffmpeg_in}" -c:v copy -c:a libmp3lame -q:a 0 -f matroska - | mkv_server 2>/dev/null 1>/dev/null
 }
 
-export -f mvkserver_out_mp3_audio
+function mvkserver_out_aac_audio () {
+    mkvserver_ffmpeg_in="-"
+    mkvserver_ffmpeg_in="${1:-${mkvserver_ffmpeg_in}}"
+    echo "ffmpeg input: ${mkvserver_ffmpeg_in}"
+    ffmpeg -re -i ${mkvserver_ffmpeg_in} -c:v copy -c:a libfdk_aac -vbr 5 -f matroska - | mkv_server 2>/dev/null 1>/dev/null
+}
+
+function mvkserver_out_passthru_audio () {
+    mkvserver_ffmpeg_in="-"
+    mkvserver_ffmpeg_in="${1:-${mkvserver_ffmpeg_in}}"
+    ffmpeg -re -i  -c:v copy -c:a copy -f matroska - | mkv_server 2>/dev/null 1>/dev/null
+}
+
+
+export -f mvkserver_out_mp3_audio 
+export -f mvkserver_out_aac_audio 
+export -f mvkserver_out_passthru_audio
