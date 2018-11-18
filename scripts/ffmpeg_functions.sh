47042be2bca59a8c1fbb9316b6c872c25fd36359
phillmac <phillmac@users.noreply.github.com>
2018-11-19 00:03:31 +1100

    Increase hls segment time

--- scripts/ffmpeg_functions.sh
+++ scripts/ffmpeg_functions.sh
@@ -8,7 +8,7 @@ function ffmpeg_hls_out () {
     ffmpeg_hls_out="${2:-${ffmpeg_hls_out}}"
     echo "ffmpeg output: ${ffmpeg_hls_out}"
     
-    ffmpeg -re -i "${ffmpeg_hls_in}" -c copy -segment_list_size 10 -hls_time 10 -hls_flags delete_segments "${ffmpeg_hls_out}"
+    ffmpeg -re -i "${ffmpeg_hls_in}" -c copy -segment_list_size 10 -hls_time 30 -hls_flags delete_segments "${ffmpeg_hls_out}"
 }
 
 export -f ffmpeg_hls_out
