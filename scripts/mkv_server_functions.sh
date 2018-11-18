941e25ba8826b2875dfa437aab0fea2ba39d68d6
phillmac <phillmac@users.noreply.github.com>
2018-11-18 19:58:35 +1100

    Update mkv_server_functions.sh
    Rename function, don't hardcode input

--- scripts/mkv_server_functions.sh
+++ scripts/mkv_server_functions.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-function serve_stream () {
-  ffmpeg -re -i  http://127.0.0.1:9000/ -c:v copy -c:a libmp3lame -q:a 0 -f matroska - | mkv_server 2>/dev/null 1>/dev/null
+function mvkserver_out_mp3_audio () {
+  mkvserver_ffmpeg_in="${1}"
+  ffmpeg -re -i "${mkvserver_ffmpeg_in}" -c:v copy -c:a libmp3lame -q:a 0 -f matroska - | mkv_server 2>/dev/null 1>/dev/null
 }
 
-
-export -f serve_stream
+export -f mvkserver_out_mp3_audio
