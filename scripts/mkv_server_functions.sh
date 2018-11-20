cec629da8f6156fdef483af8021fd8ebb50e3371
phillmac <phillmac@users.noreply.github.com>
2018-11-20 14:22:42 +1100

    Cleanup formatting, fix missing input param

--- scripts/mkv_server_functions.sh
+++ scripts/mkv_server_functions.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
 
 function mvkserver_out_mp3_audio () {
-    mkvserver_ffmpeg_in="${mkvserver_ffmpeg_in:-\-}"
+    mkvserver_ffmpeg_in="${mkvserver_ffmpeg_in:--}"
     mkvserver_ffmpeg_in="${1:-${mkvserver_ffmpeg_in}}"
     echo "ffmpeg input: ${mkvserver_ffmpeg_in}"
-    ffmpeg -re -i "${mkvserver_ffmpeg_in}" -c:v copy -c:a libmp3lame -q:a 0 -f matroska - | mkv_server 2>/dev/null 1>/dev/null
+    ffmpeg -re -i "${mkvserver_ffmpeg_in}" \
+        -c:v copy \
+        -c:a libmp3lame \
+        -q:a 0 \
+        -f matroska - | mkv_server 2>/dev/null 1>/dev/null
 }
 
 function mvkserver_out_aac_audio () {
-    mkvserver_ffmpeg_in="${mkvserver_ffmpeg_in:-\-}"
+    mkvserver_ffmpeg_in="${mkvserver_ffmpeg_in:--}"
     mkvserver_ffmpeg_in="${1:-${mkvserver_ffmpeg_in}}"
     echo "ffmpeg input: ${mkvserver_ffmpeg_in}"
-    ffmpeg -re -i ${mkvserver_ffmpeg_in} -c:v copy -c:a libfdk_aac -b:a 192k -f matroska - | mkv_server 2>/dev/null 1>/dev/null
+    ffmpeg -re -i ${mkvserver_ffmpeg_in} \
+        -c:v copy \
+        -c:a libfdk_aac \
+        -b:a 192k -f matroska - | mkv_server 2>/dev/null 1>/dev/null
 }
 
+
 function mvkserver_out_passthru_audio () {
-    mkvserver_ffmpeg_in="${mkvserver_ffmpeg_in:-\-}"
+    mkvserver_ffmpeg_in="${mkvserver_ffmpeg_in:--}"
     mkvserver_ffmpeg_in="${1:-${mkvserver_ffmpeg_in}}"
     echo "ffmpeg input: ${mkvserver_ffmpeg_in}"
-    ffmpeg -re -i  -c:v copy -c:a copy -f matroska - | mkv_server 2>/dev/null 1>/dev/null
+    ffmpeg -re -i ${mkvserver_ffmpeg_in} \
+        -c:v copy \
+        -c:a copy -f matroska - | mkv_server 2>/dev/null 1>/dev/null
 }
 
 
