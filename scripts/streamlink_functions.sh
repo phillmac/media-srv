e4c26aafddb617c358313d53838f5c29b4987bbf
phillmac <phillmac@users.noreply.github.com>
2018-11-21 12:35:00 +1100

    Cleanup params, don't clobber, add functions
    Add functions:
    streamlink_hls_mkvserver_out
    streamlink_mkvserver_out
    streamlink_mkvserver_passthru_out
    streamlink_hls_mkvserver_ice_out

--- scripts/streamlink_functions.sh
+++ scripts/streamlink_functions.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 function streamlink_http_out () {
-    streamlink_url="${1}"
-    streamlink_quality="${2}"
+    streamlink_url="${1:-${streamlink_url}}"
+    streamlink_quality="${2:-${streamlink_quality}}"
 
-    streamlink_http_port="9000"
-    streamlink_http_port="${STREAMLINK_HTTP_PORT:-${streamlink_http_port}}"
+    streamlink_http_port="${streamlink_http_port:-9000}"
     streamlink_http_port="${3:-${streamlink_http_port}}"
 
     streamlink \
@@ -19,8 +18,8 @@ function streamlink_http_out () {
 }
 
 function streamlink_player_out () {
-    streamlink_url="${1}"
-    streamlink_quality="${2}"
+    streamlink_url="${1:-${streamlink_url}}"
+    streamlink_quality="${2:-${streamlink_quality}}"
     streamlink_player_command="${@:3}";
 
     streamlink \
@@ -32,8 +31,8 @@ function streamlink_player_out () {
         "${streamlink_url}" "${streamlink_quality}"
 }
 function streamlink_stdout () {
-    streamlink_url="${1}"
-    streamlink_quality="${2}"
+    streamlink_url="${1:-${streamlink_url}}"
+    streamlink_quality="${2:-${streamlink_quality}}"
 
     streamlink \
         --hls-segment-threads 3 \
@@ -43,6 +42,64 @@ function streamlink_stdout () {
         "${streamlink_url}" "${streamlink_quality}"
 }
 
+function streamlink_hls_mkvserver_out () {
+    streamlink_url="${1:-${streamlink_url}}"
+    streamlink_quality="${2:-${streamlink_quality}}"
+    ffmpeg_hls_out="${3:-${ffmpeg_hls_out}}"
+    
+    
+    create_split_files 2
+    
+    ffmpeg_hls_in="${output_file_list[0]}"
+    mkvserver_ffmpeg_in="${output_file_list[1]}"
+    
+    #export ffmpeg_hls_in mkvserver_ffmpeg_in ffmpeg_hls_out
+    
+    tmux split bash -c "ffmpeg_hls_out ${ffmpeg_hls_in} ${ffmpeg_hls_out}"
+    tmux split bash -c "mvkserver_out_aac_audio ${mkvserver_ffmpeg_in}"
+    tmux select-layout even-vertical
+    streamlink_stdout "${streamlink_url}" "${streamlink_quality}" | split_output
+}
+
+function streamlink_hls_mkvserver_ice_out () {
+    streamlink_url="${1:-${streamlink_url}}"
+    streamlink_quality="${2:-${streamlink_quality}}"
+    ffmpeg_hls_out="${3:-${ffmpeg_hls_out}}"
+    ffmpeg_icecast_out="${4:-${ffmpeg_icecast_out}}"
+    
+    create_split_files 3
+    
+    ffmpeg_hls_in="${output_file_list[0]}"
+    mkvserver_ffmpeg_in="${output_file_list[1]}"
+    ffmpeg_icecast_in="${output_file_list[2]}"
+    
+    #export ffmpeg_hls_in ffmpeg_hls_out mkvserver_ffmpeg_in ffmpeg_hls_out ffmpeg_icecast_in ffmpeg_icecast_out
+    
+    tmux split bash -c "ffmpeg_hls_out ${ffmpeg_hls_in} ${ffmpeg_hls_out}"
+    tmux split bash -c "mvkserver_out_aac_audio ${mkvserver_ffmpeg_in}"
+    tmux split bash -c "ffmpeg_mp3_icecast_out ${ffmpeg_icecast_in} ${ffmpeg_icecast_out}"
+    tmux select-layout even-vertical
+    streamlink_stdout "${streamlink_url}" "${streamlink_quality}" | split_output
+}
+
+function streamlink_mkvserver_out () {
+    streamlink_url="${1:-${streamlink_url}}"
+    streamlink_quality="${2:-${streamlink_quality}}"
+    
+    streamlink_player_out "${streamlink_url}" "${streamlink_quality}" bash -c mvkserver_out_aac_audio
+}
+
+function streamlink_mkvserver_passthru_out () {
+    streamlink_url="${1:-${streamlink_url}}"
+    streamlink_quality="${2:-${streamlink_quality}}"
+    
+    streamlink_player_out "${streamlink_url}" "${streamlink_quality}" bash -c mvkserver_out_passthru_audio
+}
+
 export -f streamlink_http_out
 export -f streamlink_player_out
 export -f streamlink_stdout
+export -f streamlink_hls_mkvserver_out
+export -f streamlink_mkvserver_out
+export -f streamlink_mkvserver_passthru_out
+export -f streamlink_hls_mkvserver_ice_out
