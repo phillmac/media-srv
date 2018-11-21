903bda024e4b4eee688a20576a842478a6e8e707
phillmac <phillmac@users.noreply.github.com>
2018-11-21 11:39:15 +1100

    Add ffmpeg_mpegts_av_icecast_out, add more params

--- scripts/ffmpeg_functions.sh
+++ scripts/ffmpeg_functions.sh
@@ -24,6 +24,7 @@ function ffmpeg_hls_out () {
         "${ffmpeg_hls_out}"
 }
 
+# Strip any video and stream any audio as mp3
 function ffmpeg_mp3_icecast_out () {
     
     ffmpeg_icecast_in="${ffmpeg_icecast_in:--}"
@@ -32,10 +33,14 @@ function ffmpeg_mp3_icecast_out () {
     
     ffmpeg_icecast_out="${2:-${ffmpeg_icecast_out}}"
     echo "ffmpeg icecast output: ${ffmpeg_icecast_out}"
+    
+    ffmpeg_icecast_br="${ffmpeg_icecast_br:-b:a\ 192k}"     
+    ffmpeg_icecast_ar="${ffmpeg_icecast_ar:-44100}"
+   
         
     ffmpeg -re -i "${ffmpeg_icecast_in}" \
         -vn \
-        -c:a libmp3lame -b:a 192k -ar 44100 \
+        -c:a libmp3lame "${ffmpeg_icecast_br}" -ar "${ffmpeg_icecast_ar}" \
         -f mp3 \
         -legacy_icecast     "${ffmpeg_legacy_icecast:-0}" \
         -ice_name           "${ffmpeg_ice_name}" \
@@ -46,5 +51,34 @@ function ffmpeg_mp3_icecast_out () {
         "${ffmpeg_icecast_out}"
 }
 
+function ffmpeg_mpegts_av_icecast_out () {
+    
+    ffmpeg_av_icecast_in="${ffmpeg_av_icecast_in:--}"
+    ffmpeg_av_icecast_in="${1:-${ffmpeg_av_icecast_in}}"
+    echo "ffmpeg av icecast input: ${ffmpeg_av_icecast_in}"
+    
+    ffmpeg_av_icecast_out="${2:-${ffmpeg_av_icecast_out}}"
+    echo "ffmpeg av icecast output: ${ffmpeg_av_icecast_out}"
+    
+    ffmpeg_av_icecast_br="${ffmpeg_icecast_br:-b:a\ 192k}"     
+    ffmpeg_av_icecast_ar="${ffmpeg_icecast_ar:-44100}"
+        
+    ffmpeg -re -i "${ffmpeg_av_icecast_in}" \
+        -c:v copy \
+        -c:a libmp3lame "${ffmpeg_av_icecast_br}" -ar "${ffmpeg_av_icecast_ar}" \
+        -f mpegts \
+        -legacy_icecast     "${ffmpeg_av_legacy_icecast:-0}" \
+        -ice_name           "${ffmpeg_av_ice_name}" \
+        -ice_description    "${ffmpeg_av_ice_description}" \
+        -ice_url            "${ffmpeg_av_ice_url}" \
+        -ice_genre          "${ffmpeg_av_ice_genre}" \
+        -content_type audio/mpeg \
+        "${ffmpeg_av_icecast_out}"
+}
+
+
+
+
 export -f ffmpeg_hls_out
 export -f ffmpeg_mp3_icecast_out
+export -f ffmpeg_mpegts_av_icecast_out
