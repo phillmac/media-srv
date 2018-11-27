1602b9337a0d606861df6e60df61a373d550852e
phillmac <phillmac@users.noreply.github.com>
2018-11-27 18:33:09 +1100

    Fix typo

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,7 +2,7 @@
 
 scripts_dir="$(realpath $(dirname ${BASH_SOURCE}))"
 
-source "{scripts_dir}"/load_functions.sh
+source "${scripts_dir}"/load_functions.sh
 
 if [[ ! -z "${hls_cleanup_patern}" ]] && rm -v "${hls_cleanup_patern}"
 
