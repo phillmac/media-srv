75e99866f2020ddeda6219970065ebe7e7bc7442
phillmac <phillmac@users.noreply.github.com>
2018-11-27 18:34:58 +1100

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,6 +4,6 @@ scripts_dir="$(realpath $(dirname ${BASH_SOURCE}))"
 
 source "${scripts_dir}"/load_functions.sh
 
-if [[ ! -z "${hls_cleanup_patern}" ]] && rm -v "${hls_cleanup_patern}"
+[[ ! -z "${hls_cleanup_patern}" ]] && rm -v "${hls_cleanup_patern}"
 
-if [[ ! -z "${media_srv_cmd}" ]] && tmux new -d -x 150 -y 50 bash -c "${media_srv_cmd}" 
+[[ ! -z "${media_srv_cmd}" ]] && tmux new -d -x 150 -y 50 bash -c "${media_srv_cmd}" || echo 'Please environment var media_srv_cmd'
