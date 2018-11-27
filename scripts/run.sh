c71fc0df3f7432e7c5614f95d3478d902968a3e5
phillmac <phillmac@users.noreply.github.com>
2018-11-27 18:45:38 +1100

    Export scripts_dir

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,3 +7,5 @@ source "${scripts_dir}"/load_functions.sh
 [[ ! -z "${hls_cleanup_patern}" ]] && rm -v "${hls_cleanup_patern}"
 
 [[ ! -z "${media_srv_cmd}" ]] && tmux new -d -x 150 -y 50 bash -c "${media_srv_cmd}" || echo 'Please set environment var media_srv_cmd'
+
+export -f scripts_dir
