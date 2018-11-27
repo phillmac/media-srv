8f0ad07eb7a7b7c7fe940b8d522ed4e15bc40663
phillmac <phillmac@users.noreply.github.com>
2018-11-27 18:52:47 +1100

    Fix export

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-scripts_dir="$(realpath $(dirname ${BASH_SOURCE}))"
+export scripts_dir="$(realpath $(dirname ${BASH_SOURCE}))"
 
 source "${scripts_dir}"/load_functions.sh
 
 [[ ! -z "${hls_cleanup_patern}" ]] && rm -v "${hls_cleanup_patern}"
 
 [[ ! -z "${media_srv_cmd}" ]] && tmux new -d -x 150 -y 50 bash -c "${media_srv_cmd}" || echo 'Please set environment var media_srv_cmd'
-
-export scripts_dir
