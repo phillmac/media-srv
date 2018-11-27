cf3ea1bf69ca1ab4063f2f886c2693a0b7957cd8
phillmac <phillmac@users.noreply.github.com>
2018-11-27 18:13:15 +1100

    Create run.sh

---
+++ scripts/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+scripts_dir="$(realpath $(dirname ${BASH_SOURCE}))"
+
+source "{scripts_dir}"/load_functions.sh
+
+if [[ ! -z "${hls_cleanup_patern}" ]] && rm -v "${hls_cleanup_patern}"
+
+if [[ ! -z "${media_srv_cmd}" ]] && tmux new -d -x 150 -y 50 bash -c "${media_srv_cmd}" 
